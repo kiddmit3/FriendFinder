847fcc64876db2477699ec992331cfb29f11f7a4
David Lac <kiddmit3@gmail.com>
2017-08-24 12:45:03 -0700

    somewhat working

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of members.
+// ===============================================================================
+
+var memArray = [
+  {
+    name: "David",
+    insta: "kiddmit3",
+    photoURL: "https://preview.ibb.co/j7Xr6k/14711299_10210820964059176_4696343520063935414_o.jpg",
+    coolness: "9000",
+    age: 27
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = memArray;
